f391adffd7ddfb164ffacb361e551db6c0646ca0
Yotzovv <ivaylo.yotsov1@gmail.com>
2024-10-01 11:14:10 +0300

    add appsettings

--- CryptoApp/CryptoApp.API/Controllers/AuthController.cs
+++ CryptoApp/CryptoApp.API/Controllers/AuthController.cs
@@ -41,14 +41,12 @@ public class AuthController : ControllerBase
         }
         
         var user = await _userManager.FindByEmailAsync(model.Email);
-        
-        var token = GenerateJwtToken(user!);
 
         _logService.LogInfo($"Login successful for user: {model.Email}");
 
-        return Ok(new AuthResponse { Token = token });
+        return Ok();
     }
-    
+
     [HttpPost("register")]
     public async Task<IActionResult> Register([FromBody] LoginDto model)
     {
@@ -65,11 +63,11 @@ public class AuthController : ControllerBase
         {
             User = user,
         };
-        
+
         try
         {
             var result = await _userManager.CreateAsync(user, model.Password);
-            
+
             if (result.Succeeded)
             {
                 _logService.LogInfo($"Registration successful for user: {model.Email}");
@@ -78,7 +76,7 @@ public class AuthController : ControllerBase
 
             var errors = string.Join(", ", result.Errors.Select(e => e.Description));
             _logService.LogError($"Registration failed for user: {model.Email}. Errors: {errors}");
-            
+
             return BadRequest(new { message = "Registration failed.", errors = result.Errors });
         }
         catch (Exception e)
@@ -86,31 +84,5 @@ public class AuthController : ControllerBase
             _logService.LogError($"Exception during registration for user: {model.Email}", e);
             throw;
         }
-    }
-    
-    private string GenerateJwtToken(AspNetUser user)
-    {
-        _logService.LogInfo($"Generating JWT token for user: {user.Email}");
-
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
-        var tokenDescriptor = new SecurityTokenDescriptor
-        {
-            Subject = new ClaimsIdentity(new Claim[]
-            {
-                new(ClaimTypes.NameIdentifier, user.Id.ToString()),
-            }),
-            Expires = DateTime.UtcNow.AddHours(1),
-            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
-            Issuer = _configuration["Jwt:Issuer"],
-            Audience = _configuration["Jwt:Audience"] 
-        };
-
-        var token = tokenHandler.CreateToken(tokenDescriptor);
-        var tokenString = tokenHandler.WriteToken(token);
-
-        _logService.LogInfo($"JWT token generated for user: {user.Email}");
-
-        return tokenString;
     }
 }
